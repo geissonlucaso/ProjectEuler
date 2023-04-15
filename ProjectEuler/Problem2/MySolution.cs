46053771c0466d8e69aab6597f5a3dd1d934792a
Geisson Oliveira <geissonlucaso@gmail.com>
2023-04-15 15:05:34 -0300

    My solution, problem 2.

---
+++ ProjectEuler/Problem2/MySolution.cs
@@ -0,0 +1,33 @@
+﻿internal class MySolution
+{
+    private static void Main(string[] args)
+    {
+        int limit = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(Fibonacci(1, 2, limit));
+
+    }
+
+    public static int Fibonacci(int n1, int n2, int limit)
+    {
+        int sum = 0;
+
+        if (n1 < limit && n2 < limit)
+        {
+            int n3 = n1 + n2;
+            int n4 = n2 + n3;
+
+            sum += n1 + n2;
+            sum += Fibonacci(n3, n4, limit);
+            if (n1 % 2 != 0)
+                sum -= n1;
+
+            if (n2 % 2 != 0)
+                sum -= n2;
+        }
+        else
+            sum = 0;
+        return sum;
+    }
+
+}
